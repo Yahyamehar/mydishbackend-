85f42e530f50845efd5ec1169148b8168f6ae295
Yahyamehar <147187179+Yahyamehar@users.noreply.github.com>
2023-12-22 23:22:33 +0000

    new api's added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import cors module
-const bcrypt = require('bcrypt');
+const express = require("express");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import cors module
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const app = express();
@@ -10,72 +10,126 @@ const port = process.env.PORT || 5000;
 
 // MySQL database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Pa$$w0rd',  
-    database: 'mydishdb',  
+  host: "localhost",
+  user: "root",
+  password: "Pa$$w0rd",
+  database: "mydishdb",
 });
 
 db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL');
+  if (err) throw err;
+  console.log("Connected to MySQL");
 });
 
 // Middleware for parsing JSON in requests
 app.use(bodyParser.json());
 
 // Use cors middleware with specified options
-app.use(cors({
-   
-}));
+app.use(cors({}));
 
 // Registration endpoint
-app.post('/api/register', async (req, res) => {
-    console.log('Received registration request:', req.body);
-    const { firstName, lastName, email, password, rePassword, phoneNumber } = req.body;
+app.post("/api/register", async (req, res) => {
+  const { firstName, lastName, email, password, rePassword, phoneNumber } =
+    req.body;
 
-    // Validation checks (you can add more as needed)
-    if (!firstName || !lastName || !email || !password || !rePassword || !phoneNumber) {
-        return res.status(400).json({ error: 'All fields are required' });
+  // Insert user into the database with all details
+  db.query(
+    "INSERT INTO app_users (first_name, last_name, password, email, phonenumber) VALUES (?, ?, ?, ?, ?)",
+    [firstName, lastName, password, email, phoneNumber],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+      res.json({ message: "User registered successfully" });
     }
+  );
+});
 
-    if (password !== rePassword) {
-        return res.status(400).json({ error: 'Passwords do not match' });
-    }
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
 
-    // Hashing the password
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    // Insert user into the database
+  try {
+    // Retrieve user from the database based on the provided email
     db.query(
-        'INSERT INTO app_users (first_name, last_name, password, email, phonenumber) VALUES (?, ?, ?, ?, ?)',
-        [firstName, lastName, hashedPassword, email, phoneNumber],
-        (err, results) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
-            res.json({ message: 'User registered successfully' });
+      "SELECT * FROM app_users WHERE email = ? AND password = ?",
+      [email, password],
+      (err, results) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Internal Server Error" });
         }
+
+        // Check if the user exists and the password matches
+        if (results.length === 0) {
+          return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        // You can include additional information in the response if needed
+        res.json({ message: "Login successful", user: results[0] });
+      }
     );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Update user profile endpoint
+// Update user profile endpoint
+app.post("/api/update-profile", async (req, res) => {
+  const { userId, newName, newLastName, newEmail, newNumber } = req.body;
+
+  console.log("Incoming data:", req.body); // Add this line
+
+  // Update user information in the database
+  db.query(
+    "UPDATE app_users SET first_name = ?, last_name = ?, email = ?, phonenumber = ? WHERE userid = ?",
+    [newName, newLastName, newEmail, newNumber, userId],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      res.json({ message: "User profile updated successfully" });
+    }
+  );
+});
+
+// API endpoint to retrieve all products
+// API endpoint to retrieve all products
+app.get("/api/products", (req, res) => {
+  const query = "SELECT * FROM products";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    res.json({ products: results }); // Wrap the results in an object for consistency
+  });
 });
 
 // Get all users endpoint
-app.get('/api/users', (req, res) => {
-    // Retrieve all users from the app_users table
-    db.query('SELECT * FROM app_users', (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
+app.get("/api/users", (req, res) => {
+  // Retrieve all users from the app_users table
+  db.query("SELECT * FROM app_users", (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
 
-        res.json({ users: results });
-    });
+    res.json({ users: results });
+  });
 });
-
-
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
